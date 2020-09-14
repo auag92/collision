f2ca5b163f0b28b6fd043e55fb5e58bd8564a9af
Apaar Shanker <ashanker9@atl1-1-01-018-36.pace.gatech.edu>
2020-09-14 17:31:50 -0400

    new structure

--- utilities.py
+++ utilities.py
@@ -45,14 +45,25 @@ def draw_im(im, title=None):
     """
     plot a 2d image
     """
-    
     plt.imshow(im, cmap="viridis")
     plt.title(title)
     plt.colorbar()
     plt.axis('off')
     plt.show()
 
+
+@curry
+def padder(inp, shape, const_val=0):
+    """
+    args :  input matrix, new shape
+
+    returns : matrix reshaped to given shape
+    """
+    ls = np.floor((shape - inp.shape) / 2).astype(int)
+    hs = np.ceil((shape - inp.shape) / 2).astype(int)
+    return np.pad(inp, ((ls[0], hs[0]), (ls[1], hs[1]), (ls[2], hs[2])), 'constant', constant_values=const_val)
     
+
 @curry
 def return_slice(x_data, cutoff=5, s=None):
     """
@@ -70,3 +81,18 @@ def return_slice(x_data, cutoff=5, s=None):
         return x_data[(s[0] - cutoff):(s[0] + cutoff+1),
                       (s[1] - cutoff):(s[1] + cutoff+1),
                       (s[2] - cutoff):(s[2] + cutoff+1)]
+    
+    
+@curry
+def write2vtk(matrix, fname="zeo.vtk"):
+    sx, sy, sz = matrix.shape
+    mx = np.max(matrix)
+    mi = np.min(matrix)
+    lines ='# vtk DataFile Version 2.0\nVolume example\nASCII\nDATASET STRUCTURED_POINTS\nDIMENSIONS %d %d %d\nASPECT_RATIO 1 1 1\nORIGIN 0 0 0\nPOINT_DATA %d\nSCALARS matlab_scalars float 1\nLOOKUP_TABLE default\n'%(sx, sy, sz, matrix.size)
+    with open(fname, 'w') as f:
+        f.write(lines)
+        for ix in range(sz):
+            v = np.ravel(matrix[:,:,ix], order="f")
+            v = ["%1.5f"%x for x in np.round(100 * v / mx)]
+            line = " ".join(v)
+            f.write(line+"\n")
